1c70c45a978177ab8e1780cb49523cf7bf11c53b
Gabriel <ga.lima2205@gmail.com>
2024-10-14 19:31:10 -0300

    up

---
+++ trilha-net-explorando-desafio/Models/Reserva.cs
@@ -0,0 +1,64 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+                throw new Exception("A Capacidade De Hóspedes Não Pode Exceder A Capacidade de Suites");  
+                
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = Suite.ValorDiaria * DiasReservados;
+            decimal desconto = valor * 0.10M;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                
+                valor = Convert.ToDecimal(valor - desconto);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("11 99999-9999","Nokia 12", "111111111111111/11",128);
+Iphone iphone = new Iphone("11 22222-2222", "Iphone 14", "222222222222222/22",256);
+
+Console.WriteLine("Smartphone Nokia");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
